b41732e29e2c8d70c1d510bfb6eeded14a253add
AarthiSaminathan <127826312+AarthiSaminathan@users.noreply.github.com>
2023-03-16 10:26:05 +0530

    Add files via upload

---
+++ Controllers/AuthorsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using myBooks.Data.Services;
+using myBooks.Data.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace myBooks.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorsController : ControllerBase
+    {
+        private AuthorsService _authorService;
+
+        public AuthorsController(AuthorsService authorsService)
+        {
+            _authorService = authorsService;
+        }
+
+        [HttpPost("add-author")]
+        public IActionResult AddBook([FromBody] AuthorVM author)
+        {
+            _authorService.AddAuthor(author);
+            return Ok();
+        }
+
+        [HttpGet("get-author-with-book-by-id/{id}")]
+        public IActionResult GetAuthorWithBooks(int id)
+        {
+            var response = _authorService.GetAuthorWithBooks(id);
+            return Ok(response);
+        }
+    }
+}
